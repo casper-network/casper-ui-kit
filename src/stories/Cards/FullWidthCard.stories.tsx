24150c85e7465e87012200d6a86e8563337fa1fe
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-03-07 09:01:09 -0800

    fix: update card component

--- src/stories/Cards/FullWidthCard.stories.tsx
+++ src/stories/Cards/FullWidthCard.stories.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import {
-  FullWidthCard,
-  FullWidthCardProps,
-} from '../../components/Cards/FullWidthCard';
+import { Card, Children } from '../../components/Cards/FullWidthCard';
 
 export default {
-  title: 'Components/FullWidthCard',
-  component: FullWidthCard,
-} as ComponentMeta<typeof FullWidthCard>;
+  title: 'Components/Card',
+  component: Card,
+} as ComponentMeta<typeof Card>;
 
 // Create a master template for mapping args to render the Button component
-const Template: ComponentStory<typeof FullWidthCard> = (
-  args: FullWidthCardProps,
-) => <FullWidthCard {...args} />;
+const Template: ComponentStory<typeof Card> = (args: Children) => (
+  <Card {...args} />
+);
 
-const Children = (
-  <>
-    <FullWidthCard.Header>Header</FullWidthCard.Header>
-    <FullWidthCard.Body>Body</FullWidthCard.Body>
-    <FullWidthCard.Footer>Footer</FullWidthCard.Footer>
-  </>
+const CardChildren = (
+  <div>
+    <Card.Header>Header</Card.Header>
+    <Card.Footer>Footer</Card.Footer>
+  </div>
 );
 
 export const BaseFullWidthCard = Template.bind({});
 
 BaseFullWidthCard.args = {
-  children: Children,
+  children: CardChildren,
 };
