e8c7ac19534fdc548f449faa178f176daa58a069
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-03-07 09:01:09 -0800

    fix: update card component

--- src/components/Cards/FullWidthCard/FullWidthCard.tsx
+++ src/components/Cards/FullWidthCard/FullWidthCard.tsx
@@ -1,81 +1,34 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
-export interface FullWidthCardProps {
+export interface Children {
   children: React.ReactNode;
 }
 
-const getChildrenOnDisplayName = (
-  children: React.ReactNode,
-  displayName: string,
-) =>
-  React.Children.map(children, (child: any) =>
-    child?.type.displayName === displayName ? child : null,
-  );
+export const Card = ({ children }: Children) => {
+  const subComponentList = Object.keys(Card);
 
-export const FullWidthCard = ({ children }: FullWidthCardProps) => {
-  const header = getChildrenOnDisplayName(children, 'Header');
-  const body = getChildrenOnDisplayName(children, 'Body');
-  const footer = getChildrenOnDisplayName(children, 'Footer');
+  const subComponents = subComponentList.map((key: string) => {
+    return React.Children.map(children, (child: any) => {
+      return child?.type.name === key ? child : null;
+    });
+  });
 
   return (
-    <InfoCardSection>
-      <InfoCardContentWrapper>
-        {header}
-        {body}
-        {footer}
-      </InfoCardContentWrapper>
-    </InfoCardSection>
+    <div className="card">{subComponents.map(component => component)}</div>
   );
 };
 
-const Header = ({ children }: { children: React.ReactNode }) => (
-  <HeaderWrapper>{children}</HeaderWrapper>
+const Header: React.FC<Children> = ({ children }) => (
+  <div className="header">{children}</div>
 );
-Header.displayName = 'Header';
-FullWidthCard.Header = Header;
+Card.Header = Header;
 
-const Body = ({ children }: { children: React.ReactNode }) => (
-  <BodyWrapper>{children}</BodyWrapper>
+const Body: React.FC<Children> = ({ children }) => (
+  <div className="body">{children}</div>
 );
-Body.displayName = 'Body';
-FullWidthCard.Body = Body;
+Card.Body = Body;
 
-const Footer = ({ children }: { children: React.ReactNode }) => (
-  <FooterWrapper>{children}</FooterWrapper>
+const Footer: React.FC<Children> = ({ children }) => (
+  <div className="body">{children}</div>
 );
-Footer.displayName = 'Footer';
-FullWidthCard.Footer = Footer;
-
-const HeaderWrapper = styled.div`
-  margin-bottom: 2rem;
-`;
-
-const BodyWrapper = styled.div`
-  height: 100%;
-  width: auto;
-  padding: 1rem 0;
-  border: none;
-`;
-
-const FooterWrapper = styled.div`
-  margin-top: 1rem;
-`;
-
-const InfoCardSection = styled.section`
-  color: black;
-  margin: 0;
-  border-radius: 0.35rem;
-  width: 100%;
-  background-color: white;
-  border: 1px solid #e3e3e9;
-  box-shadow: 0px 0.125rem 0.5rem rgba(127, 128, 149, 0.2);
-`;
-
-const InfoCardContentWrapper = styled.div`
-  width: 100%;
-  padding: 2rem;
-  margin: 0 auto;
-  overflow-x: auto;
-  max-width: calc(100vw - 4rem);
-`;
+Card.Footer = Footer;
