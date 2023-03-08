a272b9601390707665f755fc948ce3c1a36d5e64
david <lostmorgan@gmail.com>
2023-03-08 09:51:49 -0500

    chore: rm dead code

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -25,7 +25,7 @@ export const Loader: React.FC<LoaderProps> = ({ size } = { size: 'lg' }) => {
   }
 
   const tempSvg = (
-    <svg data-testid="loader" viewBox="3 3 18 18">
+    <svg viewBox="3 3 18 18">
       <path
         fill="#0421CB"
         d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
@@ -40,7 +40,6 @@ export const Loader: React.FC<LoaderProps> = ({ size } = { size: 'lg' }) => {
   return (
     <LoaderWrapper>
       <LoaderStatus size={loaderSize} aria-label="Loading..." role="status">
-        {/* <LoaderIcon data-testid="loader" size={loaderSize} /> */}
         <LoaderIcon data-testid="loaderSvgWrapper" size={loaderSize}>
           {tempSvg}
         </LoaderIcon>
