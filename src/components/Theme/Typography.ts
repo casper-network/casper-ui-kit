fbc40349e1dde646e49a30c982d898aa320ad0a7
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-03-08 10:51:35 -0800

    feat: add typography file

---
+++ src/components/Theme/Typography.ts
@@ -0,0 +1,35 @@
+export const Typography = {
+  fonts: {
+    // change in the future if body/header fonts are specified
+    Inter: 'Inter, sans-serif',
+    Lausanne: 'Lausanne, sans-serif',
+    HelveticaNeue: 'Helvetica Neue, sans-serif',
+    CasperSans: 'Casper Sans, sans-serif',
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '3.75rem',
+    '7xl': '4.5rem',
+    '8xl': '6rem',
+    '9xl': '8rem',
+  },
+  fontWeights: {
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+  },
+  lineHeights: {},
+  letterSpacing: {},
+};
