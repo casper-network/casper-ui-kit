14df52546c401930684eebba1b4ba15d0a928ff6
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-03-08 10:50:09 -0800

    feat: add colors file

---
+++ src/components/Theme/Colors.ts
@@ -0,0 +1,21 @@
+export const NormalPalette = {
+  primary: {
+    CasperRed: '#FF2D2E',
+    Black: '#000000',
+    CasperWhite: '#F4F4F4',
+  },
+  secondary: {
+    CasperYellow: '#BCFC08',
+    CasperGreen: '#3EDC64',
+    CasperPurple: '#9F23E1',
+    CasperBlue: '#2230F0',
+    CasperBrown: '#4F282B',
+    CasperBrightPink: '#F622E6',
+    CasperPalePink: '#F9AACA',
+    White: '#FFFFF',
+  },
+  lowContrastSecondary: {
+    CasperLightPurple: '#AD53E5',
+    CasperLightBlue: '#4589F6',
+  },
+};
