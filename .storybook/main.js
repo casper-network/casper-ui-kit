ee3f30bfadd3646d668dbba48763cc46c7ba6e7d
bryce-gft <bryce.sayers-kwan@greenfence.com>
2023-03-09 16:18:51 -0700

    refactor: support svg imports in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,32 @@
 module.exports = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-jest', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-jest',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
+
+  webpackFinal: config => {
+    const fileLoaderRule = config.module.rules.find(
+      rule => rule.test && rule.test.test('.svg'),
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
+  },
 };
