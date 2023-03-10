e3679e75640fc2890ff9189befc84bfdb58ffd25
bryce-gft <bryce.sayers-kwan@greenfence.com>
2023-03-10 12:49:43 -0700

    feat: create sub components helper util

---
+++ src/utils/sub-component-helper.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+
+/**
+ * Used to render ONLY sub component children and their children under the correct child
+ * For context:
+ * https://stackoverflow.com/questions/75242208/typescript-property-type-does-not-exist-on-type-string
+ */
+export const subComponentHelper = (
+  subComponentList: string[],
+  children: React.ReactNode,
+) => {
+  return subComponentList
+    .map((key: string) => {
+      return React.Children.map(children, (child: React.ReactNode) => {
+        // don't allow any other children apart from declared sub-components render
+        if (!child || typeof child !== 'object' || !('type' in child)) {
+          return null;
+        }
+
+        if (typeof child.type === 'string') {
+          return child;
+        }
+
+        return child.type.name === key ? child : null;
+      });
+    })
+    .filter(child => !!child);
+};
