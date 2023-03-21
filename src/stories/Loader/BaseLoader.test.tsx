cfd8f5dad65167e96e62e80e8b5f4c400e334de0
david <lostmorgan@gmail.com>
2023-03-21 11:06:12 -0400

    refactor: BaseLoader test

--- src/stories/Loader/BaseLoader.test.tsx
+++ src/stories/Loader/BaseLoader.test.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react';
 
 import { Base } from './Loader.stories';
 
-it('Checks if the form is valid', () => {
-  const { getByTestId } = render(<Base {...Base.args} />);
+describe('Loader', () => {
+  it('renders base loader', () => {
+    const { getByTestId } = render(<Base {...Base.args} />);
 
-  const loaderWrapper = getByTestId('loaderId');
-  expect(loaderWrapper).toBeInTheDocument();
+    const loaderWrapper = getByTestId('loaderId');
+    expect(loaderWrapper).toBeInTheDocument();
+  });
 });
