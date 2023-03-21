e3e0a1b0e99a3f67d2e659ec2ca279b7380fa5e1
DorianKwan <bryce.sayers-kwan@live.ca>
2023-03-21 13:02:02 -0600

    refactor: polish loader story

--- src/stories/Loader/Loader.stories.tsx
+++ src/stories/Loader/Loader.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { Meta, StoryFn } from '@storybook/react';
+import { pxToRem } from '../../utils';
 import { Loader, LoaderProps } from '../../components/Loader';
 
 export default {
@@ -9,9 +11,18 @@ export default {
 
 // Create a master template for mapping args to render the Loader component
 const Template: StoryFn<typeof Loader> = (args: LoaderProps) => (
-  <Loader {...args} />
+  <StoryContainer>
+    <Loader {...args} />
+  </StoryContainer>
 );
 
 export const Base = Template.bind({});
 
 Base.args = {};
+
+const StoryContainer = styled.div`
+  width: 100%;
+  max-width: ${pxToRem(400)};
+  padding: 3rem;
+  background: white;
+`;
