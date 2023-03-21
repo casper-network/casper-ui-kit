c42e8f31a6ca86e5e464595a97fef2b04b8d7c61
david <lostmorgan@gmail.com>
2023-03-21 16:22:17 -0400

    refactor: encapsulate button tests

--- src/stories/Button/Button.test.tsx
+++ src/stories/Button/Button.test.tsx
@@ -3,36 +3,38 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import { Base, DisabledButton, RedButton } from './Button.stories';
 
-test('should render the base button', () => {
-  render(
-    <Base type="button" {...Base.args}>
-      button
-    </Base>,
-  );
-  expect(screen.getByRole('button')).toHaveTextContent('button');
-  expect(screen.getByRole('button')).toHaveStyle('background-color: blue');
-  expect(screen.getByRole('button')).toBeInTheDocument();
-});
-test('should render red button', () => {
-  render(
-    <RedButton type="button" {...Base.args}>
-      button
-    </RedButton>,
-  );
-  expect(screen.getByRole('button')).toHaveTextContent('button');
-  // expect(screen.getByRole('button')).toHaveStyle('background-color: red');
-  // expect(screen.getByRole('button')).toHaveStyle(
-  //   `background-color: ${RedButton.args?.color!}`,
-  // );
-  expect(screen.getByRole('button')).toBeInTheDocument();
-});
-test('should render the disabled button', () => {
-  render(
-    <DisabledButton type="button" {...Base.args}>
-      button
-    </DisabledButton>,
-  );
+describe('Button', () => {
+  it('should render the base button', () => {
+    render(
+      <Base type="button" {...Base.args}>
+        button
+      </Base>,
+    );
+    expect(screen.getByRole('button')).toHaveTextContent('button');
+    expect(screen.getByRole('button')).toHaveStyle('background-color: blue');
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+  it('should render red button', () => {
+    render(
+      <RedButton type="button" {...Base.args}>
+        button
+      </RedButton>,
+    );
+    expect(screen.getByRole('button')).toHaveTextContent('button');
+    // expect(screen.getByRole('button')).toHaveStyle('background-color: red');
+    // expect(screen.getByRole('button')).toHaveStyle(
+    //   `background-color: ${RedButton.args?.color!}`,
+    // );
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+  it('should render the disabled button', () => {
+    render(
+      <DisabledButton type="button" {...Base.args}>
+        button
+      </DisabledButton>,
+    );
 
-  expect(screen.getByRole('button')).toHaveTextContent('button');
-  expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toHaveTextContent('button');
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
 });
