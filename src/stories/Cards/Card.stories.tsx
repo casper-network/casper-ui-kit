eb933e4402685286fc5e4e3193a69610bac7782a
DorianKwan <bryce.sayers-kwan@live.ca>
2023-03-21 12:41:11 -0600

    refactor: polish card story

--- src/stories/Cards/Card.stories.tsx
+++ src/stories/Cards/Card.stories.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { ComponentStory } from '@storybook/react';
+import styled from '@emotion/styled';
+import { Meta, StoryFn } from '@storybook/react';
 import { Card, CardProps } from '../../components/Cards/Card';
 
 export default {
   title: 'Components/Card',
   component: Card,
-} as ComponentMeta<typeof Card>;
+} as Meta<typeof Card>;
 
 // Create a master template for mapping args to render the Button component
-const Template: ComponentStory<typeof Card> = (args: CardProps) => (
-  <Card {...args}>
-    <Card.Header>Header</Card.Header>
-    <Card.Body>Body</Card.Body>
-    <Card.Footer>Footer</Card.Footer>
-  </Card>
+const Template: StoryFn<typeof Card> = (args: CardProps) => (
+  <StoryContainer>
+    <Card {...args}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>Body</Card.Body>
+      <Card.Footer>Footer</Card.Footer>
+    </Card>
+  </StoryContainer>
 );
 
-export const BaseFullWidthCard = Template.bind({});
+export const BaseCard = Template.bind({});
 
-BaseFullWidthCard.args = {};
+BaseCard.args = {};
+
+const StoryContainer = styled.div`
+  width: 100%;
+  max-width: 400px;
+  padding: 2rem;
+`;
