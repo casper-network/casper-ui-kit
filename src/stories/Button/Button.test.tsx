98b89adf1b1abd5eada6b38629016f347fa68bc3
david <lostmorgan@gmail.com>
2023-03-22 13:00:11 -0400

    refactor: button test

--- src/stories/Button/Button.test.tsx
+++ src/stories/Button/Button.test.tsx
@@ -7,31 +7,31 @@ describe('Button', () => {
   it('should render the base button', () => {
     render(
       <Base type="button" {...Base.args}>
-        button
+        base button
       </Base>,
     );
-    expect(screen.getByRole('button')).toHaveTextContent('button');
+    expect(screen.getByRole('button')).toHaveTextContent('base button');
     expect(screen.getByRole('button')).toHaveStyle('background-color: blue');
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
   it('should render the red button', () => {
     render(
       <RedButton type="button" {...RedButton.args}>
-        button
+        red button
       </RedButton>,
     );
-    expect(screen.getByRole('button')).toHaveTextContent('button');
+    expect(screen.getByRole('button')).toHaveTextContent('red button');
     expect(screen.getByRole('button')).toHaveStyle('background-color: red');
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
   it('should render the disabled button', () => {
     render(
       <DisabledButton type="button" {...DisabledButton.args}>
-        button
+        disabled button
       </DisabledButton>,
     );
 
-    expect(screen.getByRole('button')).toHaveTextContent('button');
+    expect(screen.getByRole('button')).toHaveTextContent('disabled button');
     expect(screen.getByRole('button')).toHaveStyle('background-color: purple');
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
