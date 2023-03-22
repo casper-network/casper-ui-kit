cb7dffaf7d733792b2e5fb56374d7f49d36cba5a
david <lostmorgan@gmail.com>
2023-03-22 13:00:30 -0400

    feat: add Checkbox.test.tsx

---
+++ src/stories/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import userEvent from '@testing-library/user-event';
+import { Base, RequiredCheckbox, DisabledCheckbox } from './Checkbox.stories';
+
+describe('Checkbox', () => {
+  it('should render the base checkbox', async () => {
+    const user = userEvent.setup();
+    render(<Base boxColor="#02c1b0" checkmarkColor="#02c1b0" {...Base.args} />);
+
+    await user.click(screen.getByRole('checkbox'));
+    expect(screen.getByRole('checkbox')).toBeEnabled();
+    expect(screen.getByRole('checkbox')).toHaveStyle(
+      'border: 0.125rem solid #02c1b0',
+    );
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+  });
+
+  it('should render the required checkbox', async () => {
+    const user = userEvent.setup();
+    render(
+      <RequiredCheckbox
+        boxColor="#FF0000"
+        checkmarkColor="#FF0000"
+        {...RequiredCheckbox.args}
+      />,
+    );
+
+    await user.click(screen.getByRole('checkbox'));
+    expect(screen.getByRole('checkbox')).toBeEnabled();
+    expect(screen.getByRole('checkbox')).toHaveStyle(
+      'border: 0.125rem solid #FF0000',
+    );
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+  });
+
+  it('should render the disabled checkbox', async () => {
+    const user = userEvent.setup();
+    render(
+      <DisabledCheckbox
+        boxColor="#F1F1F4"
+        checkmarkColor="#F1F1F4"
+        {...DisabledCheckbox.args}
+      />,
+    );
+
+    await user.click(screen.getByRole('checkbox'));
+    expect(screen.getByRole('checkbox')).toBeDisabled();
+    expect(screen.getByRole('checkbox')).toHaveStyle(
+      'border: 0.125rem solid #F1F1F4',
+    );
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+  });
+});
