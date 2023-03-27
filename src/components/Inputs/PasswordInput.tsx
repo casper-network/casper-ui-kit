59fbc048cb84b98978a6777a3e985ed9d933bab9
david <lostmorgan@gmail.com>
2023-03-27 16:50:00 -0400

    refactor: add props and corresponding styles

--- src/components/Inputs/PasswordInput.tsx
+++ src/components/Inputs/PasswordInput.tsx
@@ -9,20 +9,23 @@ export interface PasswordInputProps {
   readonly label?: string;
   readonly labelPosition?: LabelPositions;
   readonly gapSize?: number;
-  readonly placeholder?: string;
   readonly fontSize?: number;
-  readonly color?: string;
+  readonly fontColor?: string;
+  readonly placeholder?: string;
+  readonly placeholderColor?: string;
+  readonly svgColor?: string;
+  readonly boxShadowColor?: string;
+  readonly focusBorderColor?: string;
+  readonly focusBorderWidth?: number;
   readonly width?: number;
   readonly height?: number;
   readonly minPasswordLength?: number;
   readonly maxPasswordLength?: number;
-  // TODO: KEEP LOOKING AT ATTRIBUTES.  CAN THERE BE JUST ONE INPUT COMPONENT?
-  // readonly pattern?: RegExp;
-  //   readonly initialChecked?: boolean;
-  //   readonly disabled?: boolean;
+  readonly pattern?: string;
+  readonly disabled?: boolean;
   readonly required?: boolean;
-  //   readonly name?: string;
-  //   readonly value?: string | number;
+  readonly name?: string;
+  readonly value?: string | number;
   readonly onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
@@ -30,21 +33,24 @@ export const PasswordInput: React.FC<PasswordInputProps> = ({
   label = 'Password Label',
   labelPosition = 'left',
   gapSize,
-  placeholder = '••••••••••••••••••••',
   fontSize,
-  color,
+  fontColor,
+  placeholder = '••••••••••••••••••••',
+  placeholderColor,
+  svgColor,
+  boxShadowColor,
+  focusBorderColor,
+  focusBorderWidth,
   width,
   height,
   minPasswordLength = 1,
   maxPasswordLength = 20,
-  // TODO: FIGURE OUT REGEX STUFF
-  // pattern = '[sS]*',
-  //   initialChecked = false,
-  //   disabled,
+  pattern,
   onChange,
-  //   value,
-  //   name,
+  value,
+  name,
   required,
+  disabled,
 }) => {
   const [passwordIsVisible, setPasswordIsVisible] = useState<boolean>();
 
@@ -56,22 +62,29 @@ export const PasswordInput: React.FC<PasswordInputProps> = ({
       <InputIconContainer>
         <StyledPasswordInput
           fontSize={fontSize}
-          color={color}
+          fontColor={fontColor}
+          boxShadowColor={boxShadowColor}
+          focusBorderColor={focusBorderColor}
+          focusBorderWidth={focusBorderWidth}
           width={width}
           height={height}
           placeholder={placeholder}
+          placeholderColor={placeholderColor}
           type={passwordIsVisible ? 'text' : 'password'}
           min={minPasswordLength}
           maxLength={maxPasswordLength}
-          // pattern={pattern}
+          pattern={pattern}
           onChange={onChange}
-          //   disabled={disabled}
-          //   value={value}
-          //   name={name}
+          value={value}
+          name={name}
           required={required}
+          disabled={disabled}
         />
-        <ViewPasswordButton onClick={() => setPasswordIsVisible(prev => !prev)}>
-          {/* TODO: ADD COLOR OPTION */}
+        <ViewPasswordButton
+          onClick={() => setPasswordIsVisible(prev => !prev)}
+          svgColor={svgColor}
+          focusBorderColor={focusBorderColor}
+          focusBorderWidth={focusBorderWidth}>
           <TogglePasswordViewIcon />
         </ViewPasswordButton>
       </InputIconContainer>
@@ -101,33 +114,73 @@ const InputIconContainer = styled.div`
   align-items: center;
   position: relative;
 `;
-// TODO: ADD COLOR TO BOX-SHADOW
-// TODO: BACKGROUND-COLOR COULD BE AN OPTION
+
 const StyledPasswordInput = styled.input<{
   fontSize?: number;
-  color?: string;
+  fontColor?: string;
+  placeholderColor?: string;
+  boxShadowColor?: string;
+  focusBorderColor?: string;
+  focusBorderWidth?: number;
   width?: number;
   height?: number;
 }>`
-  font-size: ${({ fontSize }) => (fontSize ? `${pxToRem(fontSize)}` : '13.75')};
-  color: ${({ color }) => color ?? 'magenta'};
+  font-size: ${({ fontSize }) =>
+    fontSize ? `${pxToRem(fontSize)}` : '0.85rem'};
+  color: ${({ fontColor }) => fontColor ?? '#000'};
   background-color: #fff;
   width: ${({ width }) => (width ? `${pxToRem(width)}` : `${pxToRem(197)}`)};
   height: ${({ height }) => (height ? `${pxToRem(height)}` : `${pxToRem(36)}`)};
-  box-shadow: inset 0px 1px 7px rgba(127, 128, 149, 0.3);
+  box-shadow: ${({ boxShadowColor }) =>
+    `inset 0px 1px 7px ${
+      boxShadowColor ? `${boxShadowColor}4c` : 'rgba(127, 128, 149, 0.3)'
+    }`};
   border-radius: ${pxToRem(8)};
-  border-style: none;
+  border: ${({ focusBorderWidth, required }) =>
+    `solid ${required ? '#FF0000' : 'transparent'} ${
+      focusBorderWidth ? pxToRem(focusBorderWidth) : '0.125rem'
+    } `};
+  border-radius: ${pxToRem(8)};
   padding-left: 1.5rem;
   -webkit-appearance: none;
   appearance: none;
+
+  &:placeholder-shown {
+    color: ${({ placeholderColor }) => placeholderColor ?? '#a4a3a1'};
+  }
+
+  &:focus {
+    border: ${({ focusBorderColor, focusBorderWidth }) =>
+      `solid ${focusBorderWidth ? pxToRem(focusBorderWidth) : '0.125rem'} ${
+        focusBorderColor ?? 'blue'
+      }`};
+    outline: none;
+  }
 `;
 
-const ViewPasswordButton = styled.button`
+const ViewPasswordButton = styled.button<{
+  svgColor?: string;
+  focusBorderColor?: string;
+  focusBorderWidth?: number;
+}>`
   background-color: transparent;
-  border: none;
+  fill: ${({ svgColor }) => svgColor ?? '#A4A3A1'};
+  border: ${({ focusBorderWidth }) =>
+    `solid transparent ${
+      focusBorderWidth ? pxToRem(focusBorderWidth) : '0.125rem'
+    } `};
   position: absolute;
-  right: 1.35rem;
+  right: 1rem;
   cursor: pointer;
   -webkit-appearance: none;
   appearance: none;
+
+  &:focus {
+    border: ${({ focusBorderColor, focusBorderWidth }) =>
+      `solid ${focusBorderWidth ? pxToRem(focusBorderWidth) : '0.125rem'} ${
+        focusBorderColor ?? 'blue'
+      }`};
+    border-radius: ${pxToRem(8)};
+    outline: none;
+  }
 `;
