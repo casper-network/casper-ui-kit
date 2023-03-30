573554f1eaddc769b532dd6e7445a8520484d008
david <lostmorgan@gmail.com>
2023-03-30 16:22:37 -0400

    feat: add StoryContainer component

--- src/stories/Inputs/PasswordInput/PasswordInput.stories.tsx
+++ src/stories/Inputs/PasswordInput/PasswordInput.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
+import styled from '@emotion/styled';
 import {
   PasswordInput,
   PasswordInputProps,
 } from '../../../components/Inputs/PasswordInput';
+import { pxToRem } from '../../../utils';
 
 export default {
   title: 'Components/PasswordInput',
@@ -12,9 +14,18 @@ export default {
 
 // Create a master template for mapping args to render the PasswordInput component
 const Template: StoryFn<typeof PasswordInput> = (args: PasswordInputProps) => (
-  <PasswordInput {...args} />
+  <StoryContainer>
+    <PasswordInput {...args} />
+  </StoryContainer>
 );
 
 export const Base = Template.bind({});
 
 Base.args = {};
+
+const StoryContainer = styled.div`
+  width: 100%;
+  max-width: ${pxToRem(400)};
+  padding: 2rem;
+  background: white;
+`;
