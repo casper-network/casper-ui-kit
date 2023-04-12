b83bcdd9442d308f09327fdf529d98e383bbf0cb
DorianKwan <bryce.sayers-kwan@live.ca>
2023-04-12 01:21:32 -0600

    fix: icon export index file

--- src/components/Icon/icons/SuccessIcon.tsx
+++ src/components/Icon/icons/SuccessIcon.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { defaultTheme } from '../../../theme';
 import { Icon, IconProps } from '../Icon';
 
-type OpenMenuIconProps = Omit<IconProps, 'icon'>;
+type SuccessIconProps = Omit<IconProps, 'icon'>;
 
-export const OpenMenuIcon: React.FC<OpenMenuIconProps> = ({
+export const SuccessIcon: React.FC<SuccessIconProps> = ({
   color,
   stroke,
   ...props
 }) => {
   return (
     <Icon
-      icon="OpenMenuIcon"
+      icon="SuccessIcon"
       {...props}
       color={color || defaultTheme.colors.secondary.CasperGreen}
       stroke={stroke || 'transparent'}
