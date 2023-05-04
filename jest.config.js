c79e226f991cbd66f561d848e99d04f423e92157
DorianKwan <bryce.sayers-kwan@live.ca>
2023-05-04 12:21:37 -0600

    feat: support src imports in jest environment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.svg$': '<rootDir>/jest/svg-transform.js',
@@ -11,5 +12,6 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/jest/__mocks__/fileMock.js',
     '\\.(css|less|scss)$': '<rootDir>/jest/__mocks__/styleMock.js',
+    '^src/(.*)$': '<rootDir>/src/$1',
   },
 };
