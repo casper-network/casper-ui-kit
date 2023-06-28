be9f871ee4f3e112556229b6f7f6e073985de9b2
Victor Alfaro <valfaro6@horizon.csueastbay.edu>
2023-06-28 14:52:30 -0700

    refactor: breakpoints out of typography

---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,11 @@
+export const breakpoints = {
+  xxs: '420px',
+  xs: '480px',
+  sm: '640px',
+  md: '768px',
+  xmd: '992px',
+  lg: '1024px',
+  xl: '1200px',
+  xxl: '1440px',
+  xxxl: '1600px',
+};
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,17 @@
 import { colors } from './colors';
 import { typography } from './typography';
+import { breakpoints } from './breakpoints';
 
 export interface ThemeSchema {
   colors: { [key: string]: any };
   typography: { [key: string]: any };
+  breakpoints: { [key: string]: any };
 }
 
 export const defaultTheme = {
   colors,
   typography,
+  breakpoints,
 };
 
 export type TestTheme = typeof defaultTheme;
